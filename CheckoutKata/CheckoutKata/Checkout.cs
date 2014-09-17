83e91d19f522c4cf8022592768d552c1d34ec3a5
Miles Pool <MilesJPool@gmail.com>
2014-09-17 18:06:28 +0100

    Implemented price lookup

---
+++ CheckoutKata/CheckoutKata/Checkout.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheckoutKata
+{
+	public class Checkout
+	{
+		private Dictionary<Char, int> priceList = new Dictionary<Char, int>()
+			{
+				{'A', 50},
+				{'B', 30},
+				{'C', 20},
+				{'D', 15}
+			};
+
+		public int BasketScan(String basket)
+		{
+			int total = 0;
+			foreach (char item in basket)
+			{
+				total += PriceLookup(item);
+			}
+			return total;
+		}
+
+		private int PriceLookup(char item)
+		{
+			int price;
+			if (priceList.TryGetValue(item, out price))
+			{
+				return price;
+			}
+			return 0;
+		}
+	}
+}
---
+++ CheckoutKata/CheckoutKata/CheckoutTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata
+{
+	public class CheckoutTests
+	{
+
+		private readonly Checkout _checkout = new Checkout();
+
+		[Test]
+		public void EmptyBasketTest()
+		{
+			Assert.That(_checkout.BasketScan(""), Is.EqualTo(0));
+		}
+
+		[Test]
+		public void ItemABasketTest()
+		{
+			Assert.That(_checkout.BasketScan("A"), Is.EqualTo(50));
+		}
+
+		[Test]
+		public void ItemBBasketTest()
+		{
+			Assert.That(_checkout.BasketScan("B"), Is.EqualTo(30));
+		}
+
+		[Test]
+		public void ItemCBasketTest()
+		{
+			Assert.That(_checkout.BasketScan("C"), Is.EqualTo(20));
+		}
+
+		[Test]
+		public void ItemDBasketTest()
+		{
+			Assert.That(_checkout.BasketScan("D"), Is.EqualTo(15));
+		}
+
+		[Test]
+		public void MultiUniqueItemsTest()
+		{
+			Assert.That(_checkout.BasketScan("ABCD"), Is.EqualTo(115));
+		}
+	}
+}
